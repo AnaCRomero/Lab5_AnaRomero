a18fc4cd00345e734bedbda2228cbd1c6042d90a
AnaCRomero <73078714+AnaCRomero@users.noreply.github.com>
2020-11-20 13:21:08 -0600

    Creacion de repositorio y algunas clases

---
+++ Lab6_AnaRomero/src/lab6_anaromero/CARRERA.java
@@ -0,0 +1,12 @@
+package lab6_anaromero;
+
+/**
+ *
+ * @author Ana Romero
+ */
+public class CARRERA {
+    private String nombre,facultdad,jefeCarrera;
+    private float costoMensualidad;
+    
+    
+}
---
+++ Lab6_AnaRomero/src/lab6_anaromero/ESTUDIANTES.java
@@ -0,0 +1,12 @@
+package lab6_anaromero;
+
+/**
+ *
+ * @author Ana Romero
+ */
+public class ESTUDIANTES {
+    private String nombre,apellido,numCuenta,genero;
+    private int edad;
+    private float mensualidad;
+    
+}
